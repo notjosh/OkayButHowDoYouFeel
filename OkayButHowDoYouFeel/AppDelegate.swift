847dbea84b36cec774e7f25be9e70116270c5c45
Joshua May <notjosh@gmail.com>
2017-02-22 21:10:00 +0100

    Initial Commit

---
+++ OkayButHowDoYouFeel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OkayButHowDoYouFeel
+//
+//  Created by joshua may on 22/2/17.
+//  Copyright © 2017 joshua may. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OkayButHowDoYouFeel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OkayButHowDoYouFeel
+//
+//  Created by joshua may on 22/2/17.
+//  Copyright © 2017 joshua may. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
